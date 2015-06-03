c26cea7712c580e507ab97089e151278b0478933
Ben Walker <benn.walker@gmail.com>
2015-06-03 11:42:22 +1000

    Half way through the tutorial

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,35 @@
 // simple-todos.js
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
   });
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current date
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
+  Template.task.events({
+    "click .toggle-checked": function(){
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function(){
+      Tasks.remove(this._id);
+    }
+  })
 }
